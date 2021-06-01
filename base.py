0f962043089cd0bfff29ae4138f8d3273ab43463
Xander Hirsch <xander@gideononline.com>
2021-06-01 16:16:28 -0700

    Query API to get disease code from name

---
+++ base.py
@@ -0,0 +1,30 @@
+"""Provides a single point for authorization and queries"""
+import os
+import requests
+
+_API_KEY = os.environ['GIDEON_API_KEY']
+_API_ORIGIN = 'https://api.gideononline.com'
+
+
+def api_query(path, params=None):
+    """Queries the GIDEON API
+
+    Args:
+        path: The path from the GIDEON domain. This string is the saem as what
+            is listed on the API docs.
+        params: A dictonary of key-value pairs to add to the URL
+
+    Returns:
+        A dictonary representing the API response
+
+    Raises:
+        ConnectionError: If the request does not return a 200 status code
+    """
+    r = requests.get(
+        _API_ORIGIN+path,
+        params=params,
+        headers={'Authorization': f'api_key {_API_KEY}'}
+    )
+    if r.status_code == 200:
+        return r.json()
+    raise ConnectionError('Could not connect to GIDEON API')
---
+++ diseases/list_diseases.py
@@ -0,0 +1,74 @@
+"""Interfaces with the GIDEON disease list"""
+
+from typing import Optional
+from gideon_api_python.base import api_query
+
+PATH_DISEASE = '/diseases'
+
+_CACHED_DISEASE_LIST = None
+
+def fetch_diseases(try_cached: bool = True):
+    """Fetches the disease list from GIDEON API
+
+    The disease list could be retrived from the API online or use
+    a recently saved cached version.
+
+    Args:
+        try_cached: Try to lookup the diease list locally first, before
+            checking the online version.
+
+    Returns:
+        A list of diseases where each entry is a dictonary.
+    """
+    global _CACHED_DISEASE_LIST
+    if _CACHED_DISEASE_LIST is None or not try_cached:
+        _CACHED_DISEASE_LIST = api_query(PATH_DISEASE)['data']
+    return _CACHED_DISEASE_LIST
+
+
+def get_disease_code_from_name(
+    search_name: str, strict_match: bool = False
+) -> Optional[bool]:
+    """Lookup GIDEON disease code from a disease name
+
+    The disease code is necessary to access disease specifics from
+    the GIDEON API.
+
+    Args:
+        seach_name: The name of the disease to search. The search is case
+            insensitive.
+
+    Returns:
+        If a disease can be unambiguously determined, a number.
+    """
+    #TODO consider adding the option of a strict name search
+    # Do all searches in lower case
+    search_name = search_name.lower()
+    possible_diseases = []
+    for disease in fetch_diseases():
+        if search_name in disease['disease'].lower():
+            possible_diseases.append(disease)
+
+    # This is currently a two step process of trying to determine correct
+    # disease code from search_name.
+    # Try to filter diseases if the search has multiple possibilities.
+    # If there are multiple options, either return false for strict matching
+    # or further filter the possible list by an exact (case insensitive) match
+
+    #TODO reduce code duplication and implement strict matching logic
+
+    if not possible_diseases:  # No diseases were matched
+        return None
+    if len(possible_diseases) == 1:
+        return possible_diseases[0]['disease_code']
+
+    # Try to narrow down possible diseases from options
+    if not strict_match:
+        possible_diseases = [
+            x for x in possible_diseases
+            if search_name == x['disease_name'].lower()
+        ]
+        if not possible_diseases:  # No diseases were matched
+            return None
+        if len(possible_diseases) == 1:
+            return possible_diseases[0]['disease_code']
