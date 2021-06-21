9d27ef4129bd969edf7903dda271cf8f1e4018e1
Xander Hirsch <xander@gideononline.com>
2021-06-21 15:10:00 -0700

    Removed confusing automatic id lookup in outbreaks

--- gideon_api/diseases/outbreaks.py
+++ gideon_api/diseases/outbreaks.py
@@ -1,5 +1,4 @@
-from gideon_api import gideon_api, CODE_OR_NAME
-from gideon_api.codes import lookup_item
+from gideon_api import gideon_api
 
 
 def outbreaks_by_year(year: int):
@@ -7,38 +6,28 @@ def outbreaks_by_year(year: int):
     return gideon_api.query_gideon_api('/diseases/outbreaks', {'year': year})
 
 
-def outbreaks_by_country_year(country: CODE_OR_NAME, year: int):
+def outbreaks_by_country_year(country_code: str, year: int):
     """https://api-doc.gideononline.com/#16616aab-57f9-477d-b611-892b7f19cd43"""
-    if isinstance(country, str):
-        country = lookup_item('countries', country)
     return gideon_api.query_gideon_api(
-        f'/diseases/outbreaks/distribution/{country}', {'year': year})
+        f'/diseases/outbreaks/distribution/{country_code}', {'year': year})
 
 
-def latest_outbreaks_by_country(country: CODE_OR_NAME):
+def latest_outbreaks_by_country(country_code: str):
     """https://api-doc.gideononline.com/#a331d79d-c265-48a3-a45a-dbca49c8d38d"""
-    if isinstance(country, str):
-        country = lookup_item('countries', country)
     return gideon_api.query_gideon_api(
-        f'/diseases/countries/{country}/latest-outbreaks')
+        f'/diseases/countries/{country_code}/latest-outbreaks')
 
 
-def outbreaks_by_disease(disease: CODE_OR_NAME):
+def outbreaks_by_disease(disease_code: int):
     """https://api-doc.gideononline.com/#f1a87f41-a9a4-414a-b6f7-d15c1c8b9331"""
-    if isinstance(disease, str):
-        disease = lookup_item('diseases', disease)
-    return gideon_api.query_gideon_api(f'/diseases/{disease}/outbreaks')
+    return gideon_api.query_gideon_api(f'/diseases/{disease_code}/outbreaks')
 
 
-def endemic_countries_by_disease(disease: CODE_OR_NAME):
+def endemic_countries_by_disease(disease_code: int):
     """https://api-doc.gideononline.com/#20bc3345-8c15-4cc7-a6a3-24c8ac0ccf49"""
-    if isinstance(disease, str):
-        disease = lookup_item('diseases', disease)
-    return gideon_api.query_gideon_api(f'/diseases/{disease}/countries')
+    return gideon_api.query_gideon_api(f'/diseases/{disease_code}/countries')
 
 
-def endemic_diseases_by_country(country: CODE_OR_NAME):
+def endemic_diseases_by_country(country_code: int):
     """https://api-doc.gideononline.com/#feab6f00-e926-49df-9a34-114cfafdf685"""
-    if isinstance(country, str):
-        country = lookup_item('countries', country)
-    return gideon_api.query_gideon_api(f'/diseases/countries/{country}')
+    return gideon_api.query_gideon_api(f'/diseases/countries/{country_code}')
